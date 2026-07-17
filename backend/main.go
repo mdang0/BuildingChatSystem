114e12177f7b9a2b611d2da3a9a5527cb4973717
mdang0 <mitchell.dang@outlook.com>
2026-07-17 11:21:40 -0400

    wrote the backend code for read/write websocket

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"net/http"
+		"log"
+		"github.com/gorilla/websocket"
+	)
 
+//define an upgrader that will req a read and write buffer size
+var upgrader = websocket.Upgrader{
+	ReadBufferSize: 1024,
+	WriteBufferSize: 1024,
+
+	//check origin of connection to allow us to make requests from our React dev server to here
+	CheckOrigin: func(r *http.Request) bool { return true },
+}
+
+func reader (conn *websocket.Conn){
+	for {
+		// read in a message 
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Println(string(p))
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+// define our websocket endpoint
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+
+// upgrade this connection to a websocket connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	reader(ws)
+}
+// listen indef for new messages coming thru WebSocket connection
+
+func setupRoutes() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Simple Server")
+    })
+}
 func main() {
 	fmt.Println("Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
